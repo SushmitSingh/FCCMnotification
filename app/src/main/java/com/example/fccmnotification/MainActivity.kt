07b3e197315fe00dea39101a466cd5e08d4a9b31
Your Name <sushmit8998@gmail.com>
2023-04-19 14:17:44 +0530

    Final Commit Tested in Android 13

--- app/src/main/java/com/example/fccmnotification/MainActivity.kt
+++ app/src/main/java/com/example/fccmnotification/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.fccmnotification
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.FirebaseApp
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
@@ -11,26 +19,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Initialize Firebase in your app
-        FirebaseApp.initializeApp(this)
-        FirebaseMessaging.getInstance().isAutoInitEnabled = true
-
-
         val sendNotificationButton: Button = findViewById(R.id.sendNotificationButton)
+        // Set up FCM
+        FirebaseMessaging.getInstance().subscribeToTopic("news")
 
-        // Set click listener for the button
+       //Click Listener for the button
         sendNotificationButton.setOnClickListener {
-            // Method to send a notification to the app
-            sendNotificationToTopic(
-                "All",
-                "Hello", "This is a test notification"
-            )
+            sendNotification("Title", "Message")
         }
     }
 
-    private fun sendNotificationToTopic(topic: String, title: String, body: String) {
-        val fcmNotificationSender = FcmNotificationSender()
-        fcmNotificationSender.sendNotificationToTopic(topic, title, body)
-    }
+    private fun sendNotification(title: String, message: String) {
+        // Create a notification channel for Android O and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channel_id"
+            val channelName = "Channel Name"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, channelName, importance)
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
 
+        // Build the notification
+        val builder = NotificationCompat.Builder(this, "channel_id")
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        // Show the notification
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(0, builder.build())
+    }
 }
--- app/src/main/java/com/example/fccmnotification/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fccmnotification/service/MyFirebaseMessagingService.kt
@@ -1,52 +1,60 @@
 package com.example.fccmnotification.service
 
+import android.Manifest
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.util.Log
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.fccmnotification.R
-import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // Handle incoming notification
-        Log.d(TAG, "Message data payload: ${remoteMessage.data}")
-
-        // Extract notification title and body
-        val title = remoteMessage.data["title"]
-        val body = remoteMessage.data["body"]
-
-        // Trigger action to display notification in your app
-        // You can customize the display logic according to your requirements
-        displayNotification(this, title, body)
+        // Handle incoming FCM message
+        val title = remoteMessage.notification?.title
+        val body = remoteMessage.notification?.body
+        if (title != null && body != null) {
+            sendNotification(title, body)
+        }
     }
 
+    private fun sendNotification(title: String, message: String) {
+        // Create a notification channel for Android O and above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "channel_id"
+            val channelName = "Channel Name"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelId, channelName, importance)
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Build the notification
+        val builder =
+            NotificationCompat.Builder(this, "channel_id").setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title).setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH).setAutoCancel(true)
+
+        // Show the notification
+        val notificationManager = NotificationManagerCompat.from(this)
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(0, builder.build())
+    }
 
     override fun onNewToken(token: String) {
-        super.onNewToken(token)
-        Log.d(TAG, "Refreshed token: $token")
-        // Subscribe to the desired topic
-        FirebaseMessaging.getInstance().subscribeToTopic("All")
-        Log.d(TAG, "Subscribed to topic: All")
-    }
-
-
-    private fun displayNotification(context: Context, title: String?, body: String?) {
-        // Build the notification
-        val notificationBuilder =
-            NotificationCompat.Builder(context).setSmallIcon(R.drawable.ic_notification)
-                .setContentTitle(title).setContentText(body).setAutoCancel(true)
-
-        // Display the notification
-        val notificationManager =
-            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(0, notificationBuilder.build())
-    }
-
-    companion object {
-        private const val TAG = "com.example.fccmnotification.service.MyFirebaseMessagingService"
+        // Handle token refresh or registration
+        // You can send the updated token to your server or perform any other logic here
     }
 }
